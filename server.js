0f8babbc30919c4eecf9f1d59d1f4b2f26110442
duha253 <duhayousef26@gmail.com>
2021-04-07 09:11:37 +0300

    lab08lab09

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ client.connect().then(() => {
 
 //declaring a port
 const PORT= process.env.PORT || 3001; //API key for locatins
-const GEO_CODE_API_KEY= process.env.GEO_CODE_API_KEY; //API key for wheather
+const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY; //API key for wheather
 const WEATHER_CODE_API_KEY = process.env.WEATHER_CODE_API_KEY; //API key for PARK
 const PARK_CODE_API_KEY= process.env.PARK_CODE_API_KEY;
 const DATABASE_URL= process.env.DATABASE_URL;//database
@@ -136,75 +136,75 @@ function handleMoviesRequest(req, res) {
     res.status(500).send('there is an error ');
   });
 }
-  //////////////ylep function ////////////
+//////////////ylep function ////////////
 
-  function handleYelpRequest(req, res) {
-    const city= req.query.search_query;
-    let page = req.query.page;
-    let numPerPage = 5;
-    let offsetResult = (page - 1) * numPerPage + 1;
-    const yelpurl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}&limit=5&offset=${offsetResult}`;
+function handleYelpRequest(req, res) {
+  const city= req.query.search_query;
+  let page = req.query.page;
+  let numPerPage = 5;
+  let offsetResult = (page - 1) * numPerPage + 1;
+  const yelpurl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}&limit=5&offset=${offsetResult}`;
 
-    if (!city) { //for empty request
-      res.status(404).send('no search query was provided');
-    }
+  if (!city) { //for empty request
+    res.status(404).send('no search query was provided');
+  }
 
-    superagent.get(yelpurl).set(`Authorization`, `Bearer ${YELP_API_KEY}`).then(resData => {
-      const yelpData = resData.body.businesses.map(business => {
-        return new Yelp(business);
-      });
-      res.status(200).send(yelpData);
-    }).catch(e => {
-      console.log('error');
-      res.status(500).send(' no restaurants listed in this area!!!!');
+  superagent.get(yelpurl).set(`Authorization`, `Bearer ${YELP_API_KEY}`).then(resData => {
+    const yelpData = resData.body.businesses.map(business => {
+      return new Yelp(business);
     });
-  }
-
-
-  // constructor function formate the location responed data
-  function Location(city, data) {
-    this.search_query = city;
-    this.formatted_query = data.display_name;
-    this.latitude = data.lat;
-    this.longitude = data.lon;
-  }
-  // constructor function formate the weather responed data
-  function Weather(data) {
-    this.forecast = data.weather.description;
-    this.time = data.datetime;
-  }
-
-  // constructor function formate the park responed data
-  function Park(data) {
-    this.name = data.name;
-    this.description = data.description;
-    this.address = `${data.addresses[0].linel} ${data.addresses[0].city} ${data.addresses[0].linel} ${data.addresses[0].statecode}  ${data.addresses[0].postalcode}  `;
-    this.fee = data.fees[0] || '0.00';
-    this.Park_url = data.url;
-  }
-  // constructor function formate the Movies responed data
-  function Movie(data) {
-    this.title = data.title;
-    this.overview = data.overview;
-    this.average_votes = data.vote_average;
-    this.total_votes = data.vote_count;
-    this.image_url = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
-    this.popularity = data.popularity;
-    this.released_on = data.release_date;
-  }
-  // constructor function formate the yalp responed data
-  function Yelp(data) {
-    this.name = data.name;
-    this.image_url = data.image_url;
-    this.price = data.price;
-    this.rating = data.rating;
-    this.url = data.url;
-  }
-
-  // Error Handler Routes
-  app.use('*', notFoundHandler);
-  function notFoundHandler(request, response) {
-    response.status(404).send('huh?');
-  }
+    res.status(200).send(yelpData);
+  }).catch(error => {
+    console.log('error');
+    res.status(500).send(' no restaurants listed in this area!!!!');
+  });
 }
+
+
+// constructor function formate the location responed data
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
+// constructor function formate the weather responed data
+function Weather(data) {
+  this.forecast = data.weather.description;
+  this.time = data.datetime;
+}
+
+// constructor function formate the park responed data
+function Park(data) {
+  this.name = data.name;
+  this.description = data.description;
+  this.address = `${data.addresses[0].linel} ${data.addresses[0].city} ${data.addresses[0].linel} ${data.addresses[0].statecode}  ${data.addresses[0].postalcode}  `;
+  this.fee = data.fees[0] || '0.00';
+  this.Park_url = data.url;
+}
+// constructor function formate the Movies responed data
+function Movie(data) {
+  this.title = data.title;
+  this.overview = data.overview;
+  this.average_votes = data.vote_average;
+  this.total_votes = data.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+  this.popularity = data.popularity;
+  this.released_on = data.release_date;
+}
+// constructor function formate the yalp responed data
+function Yelp(data) {
+  this.name = data.name;
+  this.image_url = data.image_url;
+  this.price = data.price;
+  this.rating = data.rating;
+  this.url = data.url;
+}
+
+// Error Handler Routes
+app.use('*', notFoundHandler);
+function notFoundHandler(request, response) {
+  response.status(404).send('huh?');
+}
+
 
